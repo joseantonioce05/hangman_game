b41ab12ab4e8fd1c502e4965eba74ec26490516c
joseantonioce05 <joseantonioce.06@gmail.com>
2021-05-15 18:00:25 -0400

    Funcion para extraer una palabra aleatoria del archivo .txt lista

--- hangman_game.py
+++ hangman_game.py
@@ -3,3 +3,22 @@
 # enumarate
 # get en diccionario
 # La sentencia os.system("cls") o os.system("clear") para limpiar la terminal
+import random
+
+
+def extract_word():
+    list_word = []
+    with open("./files/data.txt", "r", encoding="utf-8") as f:
+        for line in f:
+            list_word.append(line)
+    w = list_word[random.randint(1, 172)]
+    return w
+
+
+def run():
+    word = extract_word()
+    print(word)
+
+
+if __name__ == '__main__':
+    run()
