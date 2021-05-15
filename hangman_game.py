9d04385bb6872fe77afe54fbc822064f2a7ea3e6
joseantonioce05 <joseantonioce.06@gmail.com>
2021-05-15 17:27:19 -0400

    Initial Commit

---
+++ hangman_game.py
@@ -0,0 +1,5 @@
+# Incorpora comprehensions, manejos de errores y manejo de archivos
+# Utiliza el archivo data.txt y leelo para obtener las palabras
+# enumarate
+# get en diccionario
+# La sentencia os.system("cls") o os.system("clear") para limpiar la terminal
