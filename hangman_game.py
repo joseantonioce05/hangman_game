24cd9d6d7769dcf0260e7f7f9f9447068fa451a3
joseantonioce05 <joseantonioce.06@gmail.com>
2021-05-15 20:20:50 -0400

    Funcion para sacar la longintud de la palabra generada de forma aleatoria lista

--- hangman_game.py
+++ hangman_game.py
@@ -11,14 +11,29 @@ def extract_word():
     with open("./files/data.txt", "r", encoding="utf-8") as f:
         for line in f:
             list_word.append(line)
-    w = list_word[random.randint(1, 172)]
+    w = list_word[random.randint(1, 171)]
     return w
+
+
+def length_word(word):
+    l = len(word)
+    contador = 1
+    len_word = ""
+    while contador < l:
+        len_word += "-"
+        contador += 1
+    return len_word
 
 
 def run():
     word = extract_word()
+    len_word = length_word(word)
+
     print(word)
+    print(len_word)
 
 
 if __name__ == '__main__':
     run()
+
+
