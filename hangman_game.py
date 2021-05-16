101879af9c7c3ab3cf4ecabd0cb3383518d339b3
joseantonioce05 <joseantonioce.06@gmail.com>
2021-05-16 10:30:05 -0400

    Funcion para que se ejecute el juego lista

--- hangman_game.py
+++ hangman_game.py
@@ -11,29 +11,48 @@ def extract_word():
     with open("./files/data.txt", "r", encoding="utf-8") as f:
         for line in f:
             list_word.append(line)
-    w = list_word[random.randint(1, 171)]
+    w = list_word[random.randint(0, 171)]
+    w = w[0:len(w) - 1]
     return w
 
 
 def length_word(word):
     l = len(word)
-    contador = 1
-    len_word = ""
+    contador = 0
+    hidden_word = ""
     while contador < l:
-        len_word += "-"
+        hidden_word += "-"
         contador += 1
-    return len_word
+    return hidden_word
 
+
+def game(word, hidden_word): 
+    c = 0
+    rayas = list(hidden_word)
+    while c < 1:
+        i = 0
+        letter = input("Escribe una letra: ")
+        assert letter == 1, "Ingrese solo una letra"
+        while i < len(word):
+            if word[i] == letter:
+                print(word[i])
+                print(rayas)
+                rayas[i] = letter
+                print(rayas)
+            i += 1
+        word_user = "".join(rayas)
+        print(word_user)
+        if word == word_user:
+            c = 1
 
 def run():
     word = extract_word()
-    len_word = length_word(word)
-
+    print(len(word))
     print(word)
-    print(len_word)
-
-
+    hidden_word = length_word(word)
+    print(hidden_word)
+    game(word, hidden_word)
+    
+    
 if __name__ == '__main__':
     run()
-
-
